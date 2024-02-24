84a0b7272ef79bbcf67527bdfbfd089d5717b426
Nguyen Van Phuc <nguyenvanphucuet@gmail.com>
2024-02-24 23:30:33 +0700

    update HangMan

--- BT2/Hangman/main.cpp
+++ BT2/Hangman/main.cpp
@@ -8,7 +8,32 @@
 #define BAD_GUESS 2
 #define MAX_BAD_GUESSES 7
 
-const std::string WORD_LIST[] = {"dog", "cat" , "human", "socialism"};
+const std::string WORD_LIST[] = {
+        "angle", "ant", "apple", "arch", "arm", "army",
+        "baby", "bag", "ball", "band", "basin", "basket", "bath", "bed", "bee", "bell", "berry",
+        "bird", "blade", "board", "boat", "bone", "book", "boot", "bottle", "box", "boy",
+        "brain", "brake", "branch", "brick", "bridge", "brush", "bucket", "bulb", "button",
+        "cake", "camera", "card",  "cart", "carriage", "cat", "chain", "cheese", "chest",
+        "chin", "church", "circle", "clock", "cloud", "coat", "collar", "comb", "cord",
+        "cow", "cup", "curtain", "cushion",
+        "dog", "door", "drain", "drawer", "dress", "drop", "ear", "egg", "engine", "eye",
+        "face", "farm", "feather", "finger", "fish", "flag", "floor", "fly",
+        "foot", "fork", "fowl", "frame", "garden", "girl", "glove", "goat", "gun",
+        "hair", "hammer", "hand", "hat", "head", "heart", "hook", "horn", "horse",
+        "hospital", "house", "island", "jewel",  "kettle", "key", "knee", "knife", "knot",
+        "leaf", "leg", "library", "line", "lip", "lock",
+        "map", "match", "monkey", "moon", "mouth", "muscle",
+        "nail", "neck", "needle", "nerve", "net", "nose", "nut",
+        "office", "orange", "oven", "parcel", "pen", "pencil", "picture", "pig", "pin",
+        "pipe", "plane", "plate", "plow", "pocket", "pot", "potato", "prison", "pump",
+        "rail", "rat", "receipt", "ring", "rod", "roof", "root",
+        "sail", "school", "scissors", "screw", "seed", "sheep", "shelf", "ship", "shirt",
+        "shoe", "skin", "skirt", "snake", "sock", "spade", "sponge", "spoon", "spring",
+        "square", "stamp", "star", "station", "stem", "stick", "stocking", "stomach",
+        "store", "street", "sun", "table", "tail", "thread", "throat", "thumb", "ticket",
+        "toe", "tongue", "tooth", "town", "train", "tray", "tree", "trousers", "umbrella",
+        "wall", "watch", "wheel", "whip", "whistle", "window", "wire", "wing", "worm",
+    };
 const int WORD_COUNT = sizeof(WORD_LIST) / sizeof(std::string);
 
 const std::string chooseWord(){
@@ -48,13 +73,13 @@ struct HangMan {
         }
     }
 
-    bool isOver(){
+    bool isOver() const {
         return (isWon() || isLost());
     }
-    bool isWon(){
+    bool isWon() const {
         return hiddenLetters == 0;
     }
-    bool isLost(){
+    bool isLost() const {
         return badGuesses >= MAX_BAD_GUESSES;
     }
     
@@ -68,7 +93,7 @@ char getInput(){
     std::cin >> s;
     return s;
 }
-void renderGame(HangMan& game){
+void renderGame(const HangMan& game){
     if (game.status == GOOD_GUESS){
         std::cout << "GOOD JOB!" << std::endl;
     }else if (game.status == BAD_GUESS){
